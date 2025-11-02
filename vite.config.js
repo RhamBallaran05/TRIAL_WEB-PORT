e4f07fa69fec0647321facafa1033e49f4f79a42
Rham Ballaran <rhamballaran14@gmail.com>
2025-11-02 11:18:40 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/Midterm_Project_Web_Portfolio_DCIT26",
+  base:"/Web-Portfolio_Midterm-Project_DCIT26",
 });
